9855c4f4201795e715a6680932f44fca76af07e0
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-11-27 04:12:06 +0100

    replace signup vendor carter

--- resources/views/vendor/carter/auth/signup.blade.php
+++ resources/views/vendor/carter/auth/signup.blade.php
@@ -1,65 +1,56 @@
-@extends('carter::stand_alone')
+@extends('site.layout.skeleton')
+
+@section('header')
+   <div class="row">
+      <div class="col-xs-12 px-sm-0 text-xs-right">
+         <div class="pt-2 pr-2">
+            <a href="{{env('APP_URL')}}/">
+               <svg id="i-close" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="8.25%">
+                  <path d="M2 30 L30 2 M30 30 L2 2" />
+               </svg>
+            </a>
+         </a>
+      </div>
+   </div>
+@stop
 
 @section('content')
-    <section class="hero is-primary is-bold is-fullheight">
-        <div class="hero-body">
-            <div class="container">
+   <div class="page-signup">
+   <div class="vertical-align">
 
-                <div class="columns">
-                    <div class="column is-4 is-offset-4">
+      <div class="signup-form">
+         <i class="icon icon-shopify"></i>
 
-                        <div class="panel">
-                            <div class="panel-heading">
+         @if (count($errors) > 0)
+            @foreach ($errors->all() as $error)
+                  <p class="pt-2">
+                     Oh, No!
+                  </p>
+                  <p class="subtitle">
+                     {{ $error }}
+                  </p>
+            @endforeach
+         @endif
 
-                                @if (count($errors) > 0)
-                                    @foreach ($errors->all() as $error)
-                                        <div class="notification is-danger">
-                                            <p class="title">
-                                                Oh, No!
-                                            </p>
-                                            <p class="subtitle">
-                                                {{ $error }}
-                                            </p>
-                                        </div>
-                                    @endforeach
-                                @endif
+         <form action="{{ route('carter.install') }}" method="post">
+            {{ csrf_field() }}
+            @if ($plans)
+               <p class="mt-3">
+                     <select name="plan">
+                        @foreach ($plans as $key => $plan)
+                           <option value="{{ $key }}">{{ trim(sprintf('%s: $%.02f %s', $plan['name'], $plan['price'], $plan['test'] ? '(TEST)' : '')) }}</option>
+                        @endforeach
+                     </select>
+               </p>
+            @endif
+               <input type="text" class="input-signup" name="shop" placeholder="*.myshopify.com"/>
 
-                                <form action="{{ route('carter.install') }}" method="post">
+            <p class="pt-2 text-xs-right">
+               <button class="btn btn-base">Install Application </button>
+            </p>
 
-                                    {{ csrf_field() }}
-
-                                    @if ($plans)
-                                    <p class="control">
-                                        <span class="select is-fullwidth">
-                                            <select name="plan">
-                                                @foreach ($plans as $key => $plan)
-                                                    <option value="{{ $key }}">{{ trim(sprintf('%s: $%.02f %s', $plan['name'], $plan['price'], $plan['test'] ? '(TEST)' : '')) }}</option>
-                                                @endforeach
-                                            </select>
-                                        </span>
-                                    </p>
-                                    @endif
-
-                                    <p class="control has-addons">
-                                        <input type="text" class="input is-expanded" name="shop" placeholder="Shop Domain"/>
-                                        <input type="text" class="input" value=".myshopify.com" disabled/>
-                                    </p>
-
-                                    <p class="control">
-                                        <input type="password" class="input" name="password" placeholder="Password"/>
-                                    </p>
-
-                                    <p class="control">
-                                        <button class="button is-primary is-fullwidth">Sign Up</button>
-                                    </p>
-
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
+         </form>
+      </div>
+   </div>
+</div>
 @stop
