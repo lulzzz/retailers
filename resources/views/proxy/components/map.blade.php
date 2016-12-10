fdfb272e5f2f8e7d540e98237e45b456b749434b
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-10 06:21:39 +0100

    beta

--- resources/views/proxy/components/map.blade.php
+++ resources/views/proxy/components/map.blade.php
@@ -1,7 +1,7 @@
 <div class="retailers-container">
   <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 pr-0">
-      <div id="retailers-list" class="retailers-search">
+      <div id="retailers-list" class="retailers--search">
         <div class="retailers-locator">
           <input type="search" name="search" class="search search--map" placeholder="Enter your City, State or Country">
         </div>
