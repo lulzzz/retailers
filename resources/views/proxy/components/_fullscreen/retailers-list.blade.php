f253d5cfbe183291dee5262edb6c5bfff8858415
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-11 23:44:05 +0100

    beta

--- resources/views/proxy/components/_fullscreen/retailers-list.blade.php
+++ resources/views/proxy/components/_fullscreen/retailers-list.blade.php
@@ -7,12 +7,16 @@
   <ul class="list disabled">
     @foreach ($retailers as $key => $value)
       <li class="li-list--index ref-location" data-iso data-logo data-name data-location data-slug>
+        <div class="float-xs-left">
         <span class="name" data-retailer-name="{{$value['name']}}">{{$value['name']}}</span><br>
         <span class="street_number">{{ $value['street_number']}}</span>
         <span class="street_address">{{ $value['street_address']}}</span><br>
         <span class="city">{{ $value['city']}}</span>,
         <span class="country"> {{ $value['country']}}</span> <span class="postcode">{{$value['postcode']}}</span><br>
+      </div>
+      <div class="float-xs-right">
         <span class="distance">{{$value['distance']}}</span><small>km away </small><br>
+      </div>
       </li>
     @endforeach
   </ul>
