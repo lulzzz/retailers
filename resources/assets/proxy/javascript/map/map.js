d55c8414b926981e69a335597fb44d85829f66e5
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-12 02:04:51 +0100

    beta

--- resources/assets/proxy/javascript/map/map.js
+++ resources/assets/proxy/javascript/map/map.js
@@ -90,16 +90,16 @@
 
 
                   if(logo) {
-                    var  logo_item = '<div class="logo"><img src="'+logo+'"></div>';
+                    var  logo_item = '<img src="'+logo+'">';
                   } else {
-                    var  logo_item = '<div class="logo"><h2>'+retailer_name+'</h2></div>';
+                    var  logo_item = '<h2>'+retailer_name+'</h2>';
                   }
 
 
                   var infowindow = new InfoBubble(
                     {
                       map: map,
-                      content: '<div class="info-window"><div class="sticker">'+logo_item+'</div><div class="storefront"><img src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location='+latitude+','+longitude+'&heading=151.78&pitch=-0.76&key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do""></div></div></div>',
+                      content: '<div class="info-window"><div class="sticker"><div class="logo">'+logo_item+'</div><div class="storefront"><img src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location='+latitude+','+longitude+'&heading=151.78&pitch=-0.76&key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do""></div></div></div>',
                       shadowStyle: 0,
                       padding: 0,
                       backgroundColor: '#fff',
