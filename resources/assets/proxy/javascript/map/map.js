44c993487e018df1c4e3dd41269589e178ae6a4a
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-12 02:03:34 +0100

    beta

--- resources/assets/proxy/javascript/map/map.js
+++ resources/assets/proxy/javascript/map/map.js
@@ -99,7 +99,7 @@
                   var infowindow = new InfoBubble(
                     {
                       map: map,
-                      content: '<div class="info-window"><div class="sticker"><div class="logo"><img src="'+logo_item+'"></div><div class="storefront"><img src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location='+latitude+','+longitude+'&heading=151.78&pitch=-0.76&key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do""></div></div></div>',
+                      content: '<div class="info-window"><div class="sticker">'+logo_item+'</div><div class="storefront"><img src="https://maps.googleapis.com/maps/api/streetview?size=600x300&location='+latitude+','+longitude+'&heading=151.78&pitch=-0.76&key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do""></div></div></div>',
                       shadowStyle: 0,
                       padding: 0,
                       backgroundColor: '#fff',
