54d47db4eee5e4c02c3048dda131b431ec5eddb1
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-13 01:44:26 +0100

    Final Tweaks

--- resources/views/proxy/components/_fullscreen/retailers-list.blade.php
+++ resources/views/proxy/components/_fullscreen/retailers-list.blade.php
@@ -8,15 +8,15 @@
     @foreach ($retailers as $key => $value)
       <li class="li-list--index ref-location" data-iso data-logo data-location data-slug>
         <div class="retailer-list--left">
-        <span class="name">{{$value['name']}}</span><br>
-        <span class="street_number">{{ $value['street_number']}}</span>
-        <span class="street_address">{{ $value['street_address']}}</span><br>
-        <span class="city">{{ $value['city']}}</span>,
-        <span class="country"> {{ $value['country']}}</span> <span class="postcode">{{$value['postcode']}}</span><br>
-      </div>
-      <div class="retailer-list--right">
-        <span class="distance">{{$value['distance']}}</span><small>km away </small><br>
-      </div>
+          <span class="name">{{$value['name']}}</span><br>
+          <span class="street_number">{{ $value['street_number']}}</span>
+          <span class="street_address">{{ $value['street_address']}}</span><br>
+          <span class="city">{{ $value['city']}}</span>,
+          <span class="country"> {{ $value['country']}}</span>          
+        </div>
+        <div class="retailer-list--right">
+          <span class="distance">{{$value['distance']}}</span><small>km away </small><br>
+        </div>
       </li>
     @endforeach
   </ul>
