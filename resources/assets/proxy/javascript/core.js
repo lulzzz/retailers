36f48132e09b60dd135f03472d85cb6aca7a83e5
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-13 18:58:26 +0100

    Final Tweaks

--- resources/assets/proxy/javascript/core.js
+++ resources/assets/proxy/javascript/core.js
@@ -2,3 +2,4 @@
 //=include ../../vendor/loadjs.js
 //=include ../../vendor/store.js
 //=include ../../vendor/list.js
+//=include ../../vendor/jquery.pjax.js
--- resources/views/proxy/layout/skeleton.blade.php
+++ resources/views/proxy/layout/skeleton.blade.php
@@ -23,6 +23,18 @@
       @yield('content')
 
 
+      <script>
+      $(function($) {
+        if ($.support.pjax) {
+          var pjaxOptions = {
+            timeout: 1200,
+            fragment: 'div[data-pjax="container"]',
+            scrollTo: false
+          };
+          $(document).pjax('a', 'div[data-pjax="container"]', pjaxOptions);
+        };
+      });
+      </script>
       @yield('script')
 
       <script>
