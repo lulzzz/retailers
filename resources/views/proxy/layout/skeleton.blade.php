83ae5b728c525e888060d3373ae94d53cebf3602
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-13 19:01:19 +0100

    Final Tweaks

--- resources/views/proxy/layout/skeleton.blade.php
+++ resources/views/proxy/layout/skeleton.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
   <!-- Stylesheet :: Retailers -->
   <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/proxy/css/stylesheet.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-  <script src="https://maps.google.com/maps/api/js?key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do"></script>
   <script src="{{env('APP_URL')}}/assets/proxy/js/core.min.js"></script>
-</head>
-<body>
 
   @if($retailers->isEmpty())
     <div class="row">
@@ -36,7 +30,7 @@
       });
       </script>
       @yield('script')
-
+      <script src="https://maps.google.com/maps/api/js?key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do"></script>
       <script>
       loadjs([
         '{{env('APP_URL')}}/assets/proxy/js/qwest.min.js',
@@ -53,6 +47,3 @@
       </script>
     </div>
   @endif
-
-</body>
-</html>
