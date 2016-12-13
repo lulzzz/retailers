bad36f641d6588b48fe6a81dbed74b558b7a5702
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-13 07:51:54 +0100

    Final Tweaks

--- resources/views/proxy/components/_fullscreen/retailers-list.blade.php
+++ resources/views/proxy/components/_fullscreen/retailers-list.blade.php
@@ -1,9 +1,4 @@
 <div class="list-retailers">
-  @if($error)
-    <div class="alert">
-      <b>No Retailers Found!</b>, We have no Retailers located in <b>{{$geo['country']}}</b>.
-    </div>
-  @endif
   <ul class="list disabled">
     @foreach ($retailers as $key => $value)
       <li class="li-list--index ref-location">
