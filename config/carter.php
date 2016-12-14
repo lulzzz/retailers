9c924b6ea09219d2aec663fa071268084fc78eba
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-14 03:44:04 +0100

    Final Tweaks

--- config/carter.php
+++ config/carter.php
@@ -32,18 +32,18 @@ return [
         'plans' => [
 
             'test' => [
-                'name' => 'Test Plan',
+                'name' => 'Beta Release (Free 14 Days)',
                 'price' => 0.99,
                 'return_url' => env('APP_URL', 'http://localhost').'/activate',
-                'trial_day' => 0,
+                'trial_day' => 14,
                 'test' => true,
             ],
 
             'basic' => [
-                'name' => 'Basic Plan',
-                'price' => 9.00,
+                'name' => 'Application Cost (Free 7 Days)',
+                'price' => 37.00,
                 'return_url' => env('APP_URL', 'http://localhost').'/activate',
-                'trial_day' => 21,
+                'trial_day' => 7,
                 'test' => false,
             ],
 
