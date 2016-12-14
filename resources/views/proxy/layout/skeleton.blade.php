b2d1c519c829b18fcf820c691e41a61750014735
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-14 18:13:51 +0100

    Final Tweak 2

--- resources/views/proxy/layout/skeleton.blade.php
+++ resources/views/proxy/layout/skeleton.blade.php
@@ -1,7 +1,8 @@
-  <!-- Stylesheet :: Retailers -->
-  <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/proxy/css/stylesheet.min.css">
+<!-- Stylesheet :: Retailers -->
+<link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/proxy/css/stylesheet.min.css">
+<div class="container-fluid"  data-pjax="container">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script src="{{env('APP_URL')}}/assets/proxy/js/core.min.js"></script>
+  <script src="{{env('APP_URL')}}/assets/proxy/js/core.min.js"></script>
   @if($retailers->isEmpty())
     <div class="row">
       <div class="col-xs-12 text-xs-center pa-3">
@@ -10,40 +11,37 @@
       </div>
     </div>
   @else
-    <div class="container-fluid"  data-pjax="container">
+    @yield('content')
 
 
-      @yield('content')
-
-
-      <script>
-      $(function($) {
-        if ($.support.pjax) {
-          var pjaxOptions = {
-            timeout: 1200,
-            fragment: 'div[data-pjax="container"]',
-            scrollTo: false
-          };
-          $(document).pjax('a', 'div[data-pjax="container"]', pjaxOptions);
+    <script>
+    $(function($) {
+      if ($.support.pjax) {
+        var pjaxOptions = {
+          timeout: 1200,
+          fragment: 'div[data-pjax="container"]',
+          scrollTo: false
         };
-      });
-      </script>
-      @yield('script')
+        $(document).pjax('a', 'div[data-pjax="container"]', pjaxOptions);
+      };
+    });
+    </script>
+    @yield('script')
 
-      <script>
-      loadjs([
-        'https://maps.google.com/maps/api/js?key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do',
-        '{{env('APP_URL')}}/assets/proxy/js/qwest.min.js',
-        '{{env('APP_URL')}}/assets/proxy/js/map_styles.min.js',
-        '{{env('APP_URL')}}/assets/proxy/js/map.min.js'],
-        {
-          success: function() {
+    <script>
+    loadjs([
+      'https://maps.google.com/maps/api/js?key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do',
+      '{{env('APP_URL')}}/assets/proxy/js/qwest.min.js',
+      '{{env('APP_URL')}}/assets/proxy/js/map_styles.min.js',
+      '{{env('APP_URL')}}/assets/proxy/js/map.min.js'],
+      {
+        success: function() {
 
-            @yield('js')
+          @yield('js')
 
-          }
         }
-      );
-      </script>
-    </div>
-  @endif
+      }
+    );
+    </script>
+  </div>
+@endif
