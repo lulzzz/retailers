6f1b0a6b7a67560ce090d11588a2db37f240dae6
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-17 20:52:47 +0100

    beta

--- app/User.php
+++ app/User.php
@@ -6,40 +6,30 @@ use Illuminate\Notifications\Notifiable;
 use \NickyWoolf\Carter\OwnsShopifyStore;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+* The attributes that are mass assignable.
+*
+* @var array
+*/
 
-class User extends Authenticatable {
+class User extends Authenticatable
+{
+    use Notifiable, OwnsShopifyStore;
 
- use Authenticatable, Notifiable, OwnsShopifyStore;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
-    'name', 'email',
-
-    // Carter Additions
-    'domain', 'shopify_id', 'access_token', 'charge_id', 'installed'
+        'name', 'email', 'password',
+        'shopify_id', 'domain', 'access_token', 'charge_id', 'installed',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
-    'password', 'remember_token'
-
-    // Carter Additions
+        'password', 'remember_token', 'access_token'
     ];
 
-
     /**
-     * Every "User" has 1 Brand.
-     *
-     * @var array
-     */
+    * Every "User" has 1 Brand.
+    *
+    * @var array
+    */
     public function brand()
     {
         return $this->hasOne('App\Brand');
