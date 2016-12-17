3e852c2a834d7aed5ea14f3b0e5efcca317c9209
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-17 20:50:46 +0100

    beta

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable {
      * @var array
      */
     protected $fillable = [
-    'name', 'email', 'password',
+    'name', 'email',
 
     // Carter Additions
     'domain', 'shopify_id', 'access_token', 'charge_id', 'installed'
@@ -29,10 +29,9 @@ class User extends Authenticatable {
      * @var array
      */
     protected $hidden = [
-    'password', 'remember_token',
+    'password', 'remember_token'
 
     // Carter Additions
-    'access_token'
     ];
 
 
