512e88a9122aa53a20fe8f05745cdc4e8743778a
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-17 23:32:56 +0100

    beta

--- resources/views/proxy/layout/skeleton.blade.php
+++ resources/views/proxy/layout/skeleton.blade.php
@@ -24,7 +24,7 @@
     loadjs([
       '{{env('APP_URL')}}/assets/proxy/js/core.min.js',
       '{{env('APP_URL')}}/assets/proxy/js/qwest.min.js',
-      '{{env('APP_URL')}}/assets/proxy/js/map_themes/greylight.min.js',
+      '{{env('APP_URL')}}/assets/proxy/js/map_themes/mapbox.min.js',
       '{{env('APP_URL')}}/assets/proxy/js/map.min.js'],
       {
         success: function() {
