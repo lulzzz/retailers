d687b49353313083b824daef5626fb0c7b2c18c7
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-18 19:13:56 +0100

    beta

--- routes/carter.php
+++ routes/carter.php
@@ -37,10 +37,7 @@ Route::group(['middleware' => 'web'], function () {
         'AuthorizedUsersController@create'
     )->name('carter.login');
 
-    Route::get(
-        'embedded/dashboard',
-        'DashboardController@index'
-    )->name('carter.dashboard');
+
 
     Route::get(
         'app/expired',
