dfd485a4473846351133f67e76cc0ad3c87a800d
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-18 01:13:32 +0100

    beta

--- resources/views/proxy/layout/skeleton.blade.php
+++ resources/views/proxy/layout/skeleton.blade.php
@@ -4,8 +4,11 @@
 <!-- Stylesheet :: Locator Styling -->
 <link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/proxy/css/stylesheet.min.css">
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script src="https://maps.google.com/maps/api/js?key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do"></script>
+<!-- Libaries :: jQuery / Google Maps -->
+<script>
+if(!window.jQuery){loadjs('//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js','jQuery');}
+loadjs('//maps.google.com/maps/api/js?key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do','Maps');
+</script>
 
 @if($retailers->isEmpty())
   <div class="row">
@@ -16,21 +19,23 @@
   </div>
 @else
   <div class="container-fluid">
-    
+
     @yield('content')
 
     <script>
-    loadjs([
-      '{{env('APP_URL')}}/assets/proxy/js/core.min.js',
-      '{{env('APP_URL')}}/assets/proxy/js/qwest.min.js',
-      '{{env('APP_URL')}}/assets/proxy/js/map_themes/greywash.min.js',
-      '{{env('APP_URL')}}/assets/proxy/js/map.min.js'],
-      {
-        success: function() {
-          @yield('js')
-        }
-      }
-    );
+    loadjs.ready('Maps', function() {
+      loadjs([
+        '{{env('APP_URL')}}/assets/proxy/js/core.min.js',
+        '{{env('APP_URL')}}/assets/proxy/js/qwest.min.js',
+        '{{env('APP_URL')}}/assets/proxy/js/map_themes/greywash.min.js'
+      ]);
+    });
+    loadjs.ready('jQuery', function() {
+      loadjs('{{env('APP_URL')}}/assets/proxy/js/map.min.js', {
+        success: function()
+        { @yield('js') }
+      });
+    });
     </script>
   </div>
 @endif
