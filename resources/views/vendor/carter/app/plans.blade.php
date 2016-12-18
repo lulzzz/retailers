73e55d6f78f1e6e0a6dfc0061d331adf6f7d551c
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-18 17:42:04 +0100

    beta

--- resources/views/vendor/carter/app/plans.blade.php
+++ resources/views/vendor/carter/app/plans.blade.php
@@ -18,7 +18,7 @@
                                 <span class="select is-fullwidth">
                                     <select name="plan">
                                         @foreach ($plans as $key => $plan)
-                                            <option value="{{ $key }}">{{ format_carter_plan($plan) }}</option>
+                                        <option value="{{ $key }}"></option>
                                         @endforeach
                                     </select>
                                 </span>
