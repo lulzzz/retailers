60a8dc1e116af2dcce0b9aa7fc5e48a5368bb0e8
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-18 17:20:03 +0100

    beta

--- routes/carter.php
+++ routes/carter.php
@@ -2,28 +2,58 @@
 
 Route::group(['middleware' => 'web'], function () {
 
-    Route::get('signup', 'ShopifyAppController@showSignupForm')->name('carter.signup');
+    Route::get(
+        'signup',
+        'InstalledAppController@index'
+    )->name('carter.signup');
 
-    Route::match(['get', 'post'], 'install', 'ShopifyAppController@install')->name('carter.install');
+    Route::match(['get', 'post'],
+        'install',
+        'InstalledAppController@create'
+    )->name('carter.install');
 
-    Route::get('register', 'ShopifyUserController@register')->name('carter.register');
+    Route::get(
+        'register',
+        'RegisteredUsersController@create'
+    )->name('carter.register');
 
-    Route::get('embedded/plans', 'RecurringChargeController@index')->name('carter.plans');
+    Route::get(
+        'embedded/plans',
+        'RecurringChargesController@index'
+    )->name('carter.plans');
 
-    Route::match(['get', 'post'], 'embedded/plans/create', 'RecurringChargeController@create')->name('carter.plan.create');
+    Route::match(['get', 'post'],
+        'embedded/plans/create',
+        'RecurringChargesController@create'
+    )->name('carter.plan.create');
 
-    Route::get('activate', 'RecurringChargeController@update')->name('carter.activate');
+    Route::get(
+        'activate',
+        'RecurringChargesController@update'
+    )->name('carter.activate');
 
-    Route::get('embedded/login', 'ShopifyUserController@login')->name('carter.login');
+    Route::get(
+        'embedded/login',
+        'AuthorizedUsersController@create'
+    )->name('carter.login');
 
-    Route::get('embedded/dashboard', 'DashboardController@index')->name('carter.dashboard');
+    Route::get(
+        'embedded/dashboard',
+        'DashboardController@index'
+    )->name('carter.dashboard');
 
-    Route::get('embedded/expired', 'ShopifyUserController@expired')->name('carter.expired');
+    Route::get(
+        'app/expired',
+        'ExpiredSessionsController@index'
+    )->name('carter.expired');
 
 });
 
-Route::group(['prefix' => 'webhook/shopify'], function () {
+Route::group(['prefix' => 'webhooks'], function () {
 
-    Route::post('uninstall', 'WebhookController@uninstall')->name('carter.uninstall');
+    Route::post(
+        'app/uninstalled',
+        'WebhooksController@uninstall'
+    )->name('carter.uninstall');
 
 });
