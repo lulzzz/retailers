221c9db94c43b8541628e7ece73129a120223187
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-19 01:54:33 +0100

    beta

--- vendor/nickywoolf/carter/src/Middleware/LoginShop.php
+++ vendor/nickywoolf/carter/src/Middleware/LoginShop.php
@@ -16,9 +16,8 @@ class LoginShop
         if ($request->get('shop')) {
             if ($request->headers->get('referer') == 'https://apps.shopify.com/locate-retailers') {
                 return redirect()->route('carter.install', $request->all());
-            } else {
-                return redirect()->route('carter.login', $request->all());
             }
+            return redirect()->route('carter.login', $request->all());
         }
         return redirect()->route('carter.expired');
     }
