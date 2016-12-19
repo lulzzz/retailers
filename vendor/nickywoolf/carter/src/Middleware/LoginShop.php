b62ecdfed286de18bf959984326961d5579fa9f3
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-19 01:32:56 +0100

    beta

--- vendor/nickywoolf/carter/src/Middleware/LoginShop.php
+++ vendor/nickywoolf/carter/src/Middleware/LoginShop.php
@@ -3,13 +3,12 @@
 namespace NickyWoolf\Carter\Middleware;
 
 use Closure;
-use Url;
 
 class LoginShop
 {
     public function handle($request, Closure $next)
     {
-        dd(URL::previous());
+        dd($request->previous());
         if (auth()->check()) {
             return $next($request);
         }
