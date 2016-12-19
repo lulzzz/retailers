9cf107cd56a6618681aaac38f8755dd44c0fb337
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-19 01:45:37 +0100

    beta

--- vendor/nickywoolf/carter/src/Middleware/LoginShop.php
+++ vendor/nickywoolf/carter/src/Middleware/LoginShop.php
@@ -8,13 +8,23 @@ class LoginShop
 {
     public function handle($request, Closure $next)
     {
+
         if (auth()->check()) {
             return $next($request);
         }
 
-        if ($request->get('shop')) {
-            return redirect()->route('carter.login', $request->all());
+        if ($request->headers->get('referer') == app('carter.app_store_url')) {
+
+            return redirect()->route('carter.install', $request->all());
+            
+        } else {
+
+            if ($request->get('shop')) {
+                return redirect()->route('carter.login', $request->all());
+            }
+
         }
+
 
         return redirect()->route('carter.expired');
     }
--- vendor/nickywoolf/carter/src/Middleware/RedirectIfLoggedIn.php
+++ vendor/nickywoolf/carter/src/Middleware/RedirectIfLoggedIn.php
@@ -12,10 +12,6 @@ class RedirectIfLoggedIn
             return $next($request);
         }
 
-        if ($request->headers->get('referer') == app('carter.app_store_url')) {
-            return redirect()->route('carter.install', $request->all());
-        }
-
         return redirect()->route('carter.dashboard');
     }
 }
