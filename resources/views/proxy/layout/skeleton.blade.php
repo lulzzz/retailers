ada6cce0e376f1af73c5a7098ad75cddb3d5b1f7
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-23 03:06:50 +0100

    Minor fixes

--- resources/views/proxy/layout/skeleton.blade.php
+++ resources/views/proxy/layout/skeleton.blade.php
@@ -1,26 +1,7 @@
 <!-- Loadjs :: Async Asset Loading -->
 <script src="{{env('APP_URL')}}/assets/proxy/js/loadjs.min.js"></script>
 <script>
-loadjs([
-  '{{ env('APP_URL') }}/assets/proxy/css/stylesheet.min.css',
-  '{{env('APP_URL')}}/assets/proxy/js/core.min.js'],{
-  success: function() {
-    if(store.get('geolocate')) {
-      var latlng = { latitude: store.get('latitude'), longitude: store.get('longitude') };
-    } else {
-      var latlng = { latitude: {{$geo['lat']}}, longitude: {{$geo['lon']}} };
-    }
-    var settings = {
-      environment: '{{env('PROXY_URL')}}',
-      element:  'locate-retailer-map',
-      latitude: latlng.latitude,
-      longitude: latlng.longitude,
-      domain: '{{$domain}}',
-      api_key: 'AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do',
-      zoom: 9
-    };
-  }
-});
+
 </script>
 @if($retailers->isEmpty())
   <div class="row">
@@ -34,6 +15,26 @@ loadjs([
     @yield('content')
   </div>
   <script>
+  loadjs([
+    '{{ env('APP_URL') }}/assets/proxy/css/stylesheet.min.css',
+    '{{env('APP_URL')}}/assets/proxy/js/core.min.js'],{
+    success: function() {
+      if(store.get('geolocate')) {
+        var latlng = { latitude: store.get('latitude'), longitude: store.get('longitude') };
+      } else {
+        var latlng = { latitude: {{$geo['lat']}}, longitude: {{$geo['lon']}} };
+      }
+      var settings = {
+        environment: '{{env('PROXY_URL')}}',
+        element:  'locate-retailer-map',
+        latitude: latlng.latitude,
+        longitude: latlng.longitude,
+        domain: '{{$domain}}',
+        api_key: 'AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do',
+        zoom: 9
+      };
+    }
+  });
   loadjs([
     '//maps.google.com/maps/api/js?key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do',
     '{{env('APP_URL')}}/assets/proxy/js/map_themes/greywash.min.js',
