c2a0f182341a8cdf2b8d69c8ac46d2cea95cd32f
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-23 03:05:08 +0100

    Minor fixes

--- resources/views/proxy/layout/skeleton.blade.php
+++ resources/views/proxy/layout/skeleton.blade.php
@@ -1,9 +1,27 @@
 <!-- Loadjs :: Async Asset Loading -->
 <script src="{{env('APP_URL')}}/assets/proxy/js/loadjs.min.js"></script>
-<script src="//maps.google.com/maps/api/js?key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do"></script>
-
-<!-- Stylesheet :: Locator Styling -->
-<link rel="stylesheet" type="text/css" href="{{ env('APP_URL') }}/assets/proxy/css/stylesheet.min.css">
+<script>
+loadjs([
+  '{{ env('APP_URL') }}/assets/proxy/css/stylesheet.min.css',
+  '{{env('APP_URL')}}/assets/proxy/js/core.min.js'],{
+  success: function() {
+    if(store.get('geolocate')) {
+      var latlng = { latitude: store.get('latitude'), longitude: store.get('longitude') };
+    } else {
+      var latlng = { latitude: {{$geo['lat']}}, longitude: {{$geo['lon']}} };
+    }
+    var settings = {
+      environment: '{{env('PROXY_URL')}}',
+      element:  'locate-retailer-map',
+      latitude: latlng.latitude,
+      longitude: latlng.longitude,
+      domain: '{{$domain}}',
+      api_key: 'AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do',
+      zoom: 9
+    };
+  }
+});
+</script>
 
 @if($retailers->isEmpty())
   <div class="row">
@@ -17,39 +35,16 @@
 
     @yield('content')
 
-    <!-- jQuery Library  -->
-
     <!-- Dependencies -->
     <script>
     loadjs([
-      '{{env('APP_URL')}}/assets/proxy/js/core.min.js',
+      '//maps.google.com/maps/api/js?key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do',
       '{{env('APP_URL')}}/assets/proxy/js/map_themes/greywash.min.js',
       '{{env('APP_URL')}}/assets/proxy/js/map.min.js'], {
-        success: function() {
-          if(store.get('geolocate')) {
-            var latlng = {
-              latitude: store.get('latitude'),
-              longitude: store.get('longitude')
-            };
-          } else {
-            var latlng = {
-              latitude: {{$geo['lat']}},
-              longitude: {{$geo['lon']}}
-            };
-          }
-          var settings = {
-            environment: '{{env('PROXY_URL')}}',
-            element:  'locate-retailer-map',
-            latitude: latlng.latitude,
-            longitude: latlng.longitude,
-            domain: '{{$domain}}',
-            api_key: 'AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do',
-            zoom: 9
-          };
-          retailers.map(settings);
-        }
-      }
-    );
-    </script>
-  </div>
-@endif
+        async: false,
+        success: function()
+        { retailers.map(settings); }
+      });
+      </script>
+    </div>
+  @endif
