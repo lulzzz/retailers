d746bcf20f114494f95d4ad2172d1435b7aad816
Nikolas Savvidis <nicos.savidis@gmail.com>
2016-12-23 03:08:33 +0100

    Minor fixes

--- resources/views/proxy/layout/skeleton.blade.php
+++ resources/views/proxy/layout/skeleton.blade.php
@@ -1,8 +1,5 @@
 <!-- Loadjs :: Async Asset Loading -->
 <script src="{{env('APP_URL')}}/assets/proxy/js/loadjs.min.js"></script>
-<script>
-
-</script>
 @if($retailers->isEmpty())
   <div class="row">
     <div class="col-xs-12 text-xs-center pa-3">
@@ -17,31 +14,30 @@
   <script>
   loadjs([
     '{{ env('APP_URL') }}/assets/proxy/css/stylesheet.min.css',
-    '{{env('APP_URL')}}/assets/proxy/js/core.min.js'],{
-    success: function() {
-      if(store.get('geolocate')) {
-        var latlng = { latitude: store.get('latitude'), longitude: store.get('longitude') };
-      } else {
-        var latlng = { latitude: {{$geo['lat']}}, longitude: {{$geo['lon']}} };
-      }
-      var settings = {
-        environment: '{{env('PROXY_URL')}}',
-        element:  'locate-retailer-map',
-        latitude: latlng.latitude,
-        longitude: latlng.longitude,
-        domain: '{{$domain}}',
-        api_key: 'AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do',
-        zoom: 9
-      };
-    }
-  });
-  loadjs([
+    '{{env('APP_URL')}}/assets/proxy/js/core.min.js',
     '//maps.google.com/maps/api/js?key=AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do',
     '{{env('APP_URL')}}/assets/proxy/js/map_themes/greywash.min.js',
     '{{env('APP_URL')}}/assets/proxy/js/map.min.js'], {
       async: false,
-      success: function()
-      { retailers.map(settings); }
+      success: function() {
+
+        if(store.get('geolocate')) {
+          var latlng = { latitude: store.get('latitude'), longitude: store.get('longitude') };
+        } else {
+          var latlng = { latitude: {{$geo['lat']}}, longitude: {{$geo['lon']}} };
+        }
+
+        var settings = {
+          environment: '{{env('PROXY_URL')}}',
+          element:  'locate-retailer-map',
+          latitude: latlng.latitude,
+          longitude: latlng.longitude,
+          domain: '{{$domain}}',
+          api_key: 'AIzaSyAMElu9QAKi3qU68wXQ5yJSCG_YNWVU3do',
+          zoom: 9
+        };
+        retailers.map(settings);
+      }
     });
     </script>
 @endif
