b815a7318177907f191ce1bf7f96093b597555ed
Nikolas Savvidis <nicos.savidis@gmail.com>
2017-01-28 04:42:30 +0100

    fx

--- resources/views/app/retailers/index.blade.php
+++ resources/views/app/retailers/index.blade.php
@@ -12,7 +12,7 @@
               <h2>Add your first Retailer!</h2>
               <h4 class="lead">There is currently no Retailers in your database.</h4>
               <img src="/images/website-icon.png" class="img-fluid bg-icons">
-              <p><a type="button" href="/retailers/create" class="btn btn-lg btn-primary">Create Retailer</button></p>
+              <p><a href="/retailers/create" class="btn btn-lg btn-primary">Create Retailer</a></p>
             </div>
           </div>
         </div>
